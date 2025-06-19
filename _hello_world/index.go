3d8c84a64893468c28ac2fb687b355ada4c709b8
meet2aman <meet2amankushwaha@gmail.com>
2025-06-19 14:17:58 +0530

    First Commit

---
+++ _hello_world/index.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	username := "Aman"
+	fmt.Println("Hello World", username)
+}
---
+++ _variables/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/// String
+	var name string = "Loki"
+	fmt.Println("This is the String", name)
+
+	/// Boolean
+	var isTrue bool = true
+	fmt.Println("This is the Boolean", isTrue)
+
+	/// General Integer
+	var z int = 25
+	fmt.Println("This is the General Number", z)
+
+	// Unsigned integer
+	var unSignedInteger uint16 = 30
+	fmt.Println("This is the Unsigned Number", unSignedInteger)
+
+	/// Signed Integer
+	var signedInteger int16 = -15
+	fmt.Println("This is the Signed Number", signedInteger)
+
+	/// float
+	var decimalNumber float32 = 24.349374873
+	fmt.Println("This is the Float Number", decimalNumber)
+
+}
